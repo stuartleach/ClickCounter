3e5fca826556961957d23034546d1232bf3f4413
Stuart Leach <jstuartleach@gmail.com>
2017-05-28 23:55:54 -0400

    began rectangles and such

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Label
+        var label = UILabel()
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        
+        // Button
+        var button = UIButton()
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        self.count = self.count + 1
+        self.label.text = "\(self.count)"
     }
-
-
 }
+
+
+
+var button = UIButton.accessibilityElementDidLoseFocus()
 
